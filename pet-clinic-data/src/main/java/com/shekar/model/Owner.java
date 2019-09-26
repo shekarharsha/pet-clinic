5b4bde5dfb54cbc183c3a813ff4ff934ba54b3bc
Harsha Shekar <harshashekar.1987@gmail.com>
2019-09-26 07:04:33 +0530

    UI layer taken from original repo

--- pet-clinic-data/src/main/java/com/shekar/model/Owner.java
+++ pet-clinic-data/src/main/java/com/shekar/model/Owner.java
@@ -1,4 +1,17 @@
 package com.shekar.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
+
 }
--- pet-clinic-data/src/main/java/com/shekar/model/Vet.java
+++ pet-clinic-data/src/main/java/com/shekar/model/Vet.java
@@ -1,4 +1,13 @@
 package com.shekar.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 }
